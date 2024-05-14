7b5aff3a09e1980984073eab59a0eea09e8da98a
Sourav@2820 <souravmukherjeeofficial216@gmail.com>
2024-05-14 14:17:58 +0530

    comments and escapesequence

---
+++ 02comments.py
@@ -0,0 +1,20 @@
+
+# SINGLE LINE COMMENTS:
+# This is syntax for single line comments '#'
+
+
+
+#MULTILINE COMMENTS:(METHOD-1)
+#We
+#can
+# use 
+# multiline comments
+# by this 
+# method
+
+#MULTILINE COMMENTS:(METHOD-2)
+# We can also add multiline comments by select the lines and press (Command+/).
+
+
+#MULTILINE COMMENTS:(METHOD-3)
+#For print a paragraph we can use this """_____your text here_______"""
---
+++ 03escapeSequence.py
@@ -0,0 +1,5 @@
+#print("This is a "Sentence") ---->This line not execute.
+
+print("This is a \"Sentence\"") #---> This line will execute.(double code escape sequence)
+
+print("This is a \'Sentence\'") #---> This line will execute.(Single code escape sequence)
