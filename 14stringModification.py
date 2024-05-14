90c35a22e8bc05d325679c25156a1fa763e8dd4d
Sourav@2820 <souravmukherjee1111999@gmail.com>
2024-05-15 01:26:16 +0530

    Update 14stringModification.py

--- 14stringModification.py
+++ 14stringModification.py
@@ -26,3 +26,24 @@ print(countstr)
 name = "Hello world!!!"
 print (name.endswith("!!!"))
 print (name.endswith("%"))
+
+
+#We can also check for a value between the string by providing start and end index position.
+str1 = "Welcome to the console"
+print(str1.endswith("to",4,10))
+
+#find()- Seaches for the first occurance of the value and return the index, if not find then return -1.
+str4 = "My name is Sourav Mukherjee "
+print(str4.find("is"))
+print(str4.find("Bhaskar"))
+
+#index()- Searches for the first occurance and return the index value. If not present then raise exception.
+str5 = "My name is Sourav Mukherjee, I pursuing Btech from Heritage Institute of Technology, Kolkata."
+print(str5.index("Heritage"))
+#print(str5.index("Electronics"))
+
+#isalnum()- This method returns True if the entire message contains "A-->Z", "a-->z","0-->9" return True, if any another types of string present return False
+str6 = "My name is Sourav Mukherjee I am 23 years old"
+print(str6.isalnum())
+str7 = "WelcomeToTheConsole"
+print(str7.isalnum())
