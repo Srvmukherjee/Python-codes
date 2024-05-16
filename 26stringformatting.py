e8defc25f0a5f91fe9fc3e7281494e16cf6a424e
Sourav@2820 <souravmukherjee1111999@gmail.com>
2024-05-16 17:22:30 +0530

    string

---
+++ 26stringformatting.py
@@ -0,0 +1,14 @@
+# String Format: 
+txt = "For only {price:.2f} rupees!"
+print(txt.format(price = 29))
+
+# fstring in python: IT also known as literal string. When we prefix f before the string the string becomee f string.
+# fstring formatted as str.format() method.
+
+val = 'Greeks'
+print(f"{val}for{val} is a portal for {val}")
+name = 'Tushar'
+age = 23
+print(f"Hello, My name is {name}, my age is {age}")
+
+print(f"{2*30}")
---
+++ 27docString.py
@@ -0,0 +1,34 @@
+# Docstring: string literals that appears right after definition of function.
+
+def square(n):
+    '''Takes a number and return square of it'''# '''This is called docstring'''
+    print(n**2)
+square(5)
+
+
+# Example 2:
+def add(num1,num2):
+    '''Add two number 
+    number 1 = num1
+    number 2 = num2
+    and return the sum of the number.'''
+    num3 = num1+num2
+    print(num3)
+add(5,3)
+print(add.__doc__)
+add(2,3)
+print(add.__doc__)
+
+# Example 3:
+
+def num(n):
+    '''This is square of any number:'''
+    print(n**2) 
+num(5)
+print(num.__doc__)
+
+
+
+
+
+
