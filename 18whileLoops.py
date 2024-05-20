e1ef2875e3e44c36fd7afc1fd56daf5f44b1da69
Sourav@2820 <souravmukherjee1111999@gmail.com>
2024-05-20 09:58:07 +0530

    Update 18whileLoops.py

--- 18whileLoops.py
+++ 18whileLoops.py
@@ -1,17 +1,37 @@
-# while loops:
-count=5
-while(count>0):
-    print(count)
-    count = count-1
+# # while loops:
+# count=5
+# while(count>0):
+#     print(count)
+#     count = count-1
 
 
-#Else with while loops:
+# #Else with while loops:
     
-count =5
-while(count>0):
-    print(count)
-    count = count-1
-else:
-    print("Counter is 0")
+# count =5
+# while(count>0):
+#     print(count)
+#     count = count-1
+# else:
+#     print("Counter is 0")
 
 
+# # # Print the number from 0 to 100, by the help of While loop:
+# num = 0
+# while(num<=100):
+#     print(num) 
+#     num += 1
+
+# # Print the number from 100 to 0, by the help of While loop:
+
+# num = 100
+# while(num>=0):
+#     print(num)
+#     num -= 1
+
+# print the multipication table of n
+
+a=int(input("Enter number"))
+for i in range(1,11):
+        print(i*a)
+        i+=1
+        
