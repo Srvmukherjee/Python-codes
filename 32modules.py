b6c99aa4ef37b538e1577c9ca484f668d8e10134
Sourav@2820 <souravmukherjee1111999@gmail.com>
2024-05-27 11:12:11 +0530

    working with modules

---
+++ 32modules.py
@@ -0,0 +1,33 @@
+# Math module
+import math
+
+print(math.sqrt(16))
+print(math.pow(2,5))
+print(dir(math))
+math.log10(100)
+
+'''4.0
+32.0
+['__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'cbrt', 'ceil', 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e', 'erf', 'erfc', 'exp', 'exp2', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'sumprod', 'tan', 'tanh', 'tau', 'trunc', 'ulp']'''
+
+
+import calendar
+cal = calendar.month(2016,1)
+print(cal)
+print(calendar.isleap(2016))
+
+
+# make my own module:
+
+import function1
+
+area1 = function1.area(6,7)
+print(area1)
+
+
+import function1 as f
+
+area1 = f.area(6,7)
+print(area1)
+
+
